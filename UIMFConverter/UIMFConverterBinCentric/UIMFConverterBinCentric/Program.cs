3dadef287f054e45c6e5cbe26f75941c38c03935
alchemistmatt <matthew.monroe@pnnl.gov>
2017-05-19 17:54:52 -0700

    Add UIMFConverter

---
+++ UIMFConverter/UIMFConverterBinCentric/UIMFConverterBinCentric/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using UIMFLibrary;
+
+namespace UIMFConverterBinCentric
+{
+    class Program
+    {
+        //args "Gly09_SN130_8Mar13_Cheetah_C14_220nL_IMS6_2700V_130C_Multi_" "D:\Csharp\ConosleApps\LocalServer\UIMF" ""S:" ".uimf"
+        static void Main(string[] args)
+        {
+
+            //input
+            string targetFolder = @"D:\Csharp\ConosleApps\LocalServer\UIMF";
+            string destinationProcessingTempFolder = @"S:";
+            string fileName = "Gly09_SN130_8Mar13_Cheetah_C14_220nL_IMS6_2700V_130C_Multi_";
+            string fileNameEnding = ".uimf";
+
+            string addOn = "BC";
+
+            if(args.Length == 4)
+            {
+                fileName = args[0];//the UIMF file of interest
+                targetFolder = args[1];//the folder where the UIMF is
+                destinationProcessingTempFolder = args[2];// where we will process the file
+                fileNameEnding = args[3];
+            }
+
+            //set up locations
+            string targetfileName = fileName + fileNameEnding;
+            string desinationFileName = fileName + "_" + addOn + fileNameEnding;
+            string target = targetFolder + "\\" + targetfileName;
+            string destination = destinationProcessingTempFolder + "\\" + desinationFileName;
+
+
+            //copy file to working location
+            XCopyFX.CopyFile.ProcessXcopyFile(target, destination);
+
+            FileInfo uimfFile = new FileInfo(destination);
+
+            DataWriter uimfWriter = new DataWriter();
+            uimfWriter.OpenUIMF(uimfFile.FullName);
+
+            uimfWriter.CreateBinCentricTables();
+
+            uimfWriter.CloseUIMF();
+
+            Console.WriteLine("Copy Back");
+
+            XCopyFX.CopyFile.ProcessXcopyFile(destination, targetFolder + "\\" + desinationFileName);
+
+            Console.WriteLine("Delete");
+
+            if (uimfFile.Exists)
+            {
+                uimfFile.Delete();
+            }
+        }
+    }
+}
---
+++ UIMFConverter/UIMFConverterBinCentric/XCopyFX/CopyFile.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace XCopyFX
+{
+    public static class CopyFile
+    {
+        public static void ProcessXcopyFolder(string SolutionDirectory, string TargetDirectory)
+        {
+            // Use ProcessStartInfo class 
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+
+            //Give the name as Xcopy 
+            startInfo.FileName = "xcopy";
+
+            //make the window Hidden 
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            //Send the Source and destination as Arguments to the process 
+            startInfo.Arguments = SolutionDirectory + " " + TargetDirectory + @" /e /y /I";
+
+            try
+            {
+                // Start the process with the info we specified. 
+                // Call WaitForExit and then the using statement will close. 
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                }
+            }
+
+            catch (Exception exp)
+            {
+                throw exp;
+            }
+        }
+
+        public static void ProcessXcopyFile(string fileNameTarget, string fileNameDestination)
+        {
+            
+            //// Use ProcessStartInfo class 
+            //ProcessStartInfo startInfo = new ProcessStartInfo();
+            //startInfo.CreateNoWindow = false;
+            //startInfo.UseShellExecute = false;
+
+            //////Give the name as Xcopy 
+            //startInfo.FileName = "cmd.exe";
+
+
+
+            //////make the window Hidden 
+            //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            //////Send the Source and destination as Arguments to the process 
+            ////startInfo.Arguments = "/C copy /b \"" + SolutionDirectory + "\" " + TargetDirectory;
+            //startInfo.Arguments = "/C copy /b \"" + fileNameTarget + "\" " + fileNameDestination;
+            //try
+            //{
+            //    // Start the process with the info we specified. 
+            //    // Call WaitForExit and then the using statement will close. 
+            //    using (Process exeProcess = Process.Start(startInfo))
+            //    {
+            //        exeProcess.WaitForExit();
+            //    }
+            //}
+
+            //catch (Exception exp)
+            //{
+            //    throw exp;
+            //}
+
+            System.IO.File.Copy(fileNameTarget, fileNameDestination, true);
+
+        }
+        
+    }
+}
