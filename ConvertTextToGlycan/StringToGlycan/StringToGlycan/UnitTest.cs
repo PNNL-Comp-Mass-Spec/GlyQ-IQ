24e7c0ddd9aa384d8cca40a8878a685312c13f90
alchemistmatt <matthew.monroe@pnnl.gov>
2017-05-19 17:25:42 -0700

    Add ConvertTextToGlycan

---
+++ ConvertTextToGlycan/StringToGlycan/StringToGlycan/UnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GlycanTextToMass;
+using NUnit.Framework;
+
+namespace StringToGlycan
+{
+    public class UnitTest
+    {
+        [Test]
+        public void convertstringToMZ()
+        {
+            string testGlycan = "5411";
+            int charge = 2;
+
+            double massToCharge;
+            GlycanTextToMass.Convert.StringToMassFX(testGlycan, charge, out massToCharge);
+
+            Assert.AreEqual(massToCharge, 1040.88785015808d);
+            Console.Write("The m/z is " + massToCharge);
+        }
+
+        
+
+    }
+}
